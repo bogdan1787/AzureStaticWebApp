05df27972b690a2a7de4a2433af1731199b4e4d6
Bogdan Enache <enachebogdan87@yahoo.com>
2020-08-27 14:32:43 +0100

    Calling the azure function

---
+++ DummyApp/src/app/main-component/main.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  resultApi: string;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+
+  click(): void {
+    this.httpClient.get('/api/function?name=test').subscribe(result =>
+      {
+          this.resultApi = result.toString();
+      });
+  }
+}
